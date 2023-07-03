b1e6eb51908b90521fc731110ffa20400af73909
Gunturaa <gunturtriatmaja@students.amikom.ac.id>
2023-07-03 22:57:33 +0700

    commit ke-12

---
+++ TugasArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrNama = { "Erik", "Wely", "Sony"};
+            int[] arrNim = { 1234, 2657, 3434 };
+            int[] arrNilai = { 75, 80, 95};
+
+
+            Console.WriteLine("No   " + "Nama     NIM     Nilai");
+            Console.WriteLine("===============================");
+
+            //menampilkan semua elemen array
+            for (int i = 0; i < arrNama.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ".   " + arrNama[i] + "     " + arrNim[i] + "     " + arrNilai[i]);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
